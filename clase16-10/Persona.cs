7912102cdfc1d3dae49ba0f2a9f975b69c706f65
dotCero <lucero.inginfo@gmail.com>
2021-10-16 15:47:15 -0300

    Agregar archivos de proyecto.

---
+++ clase16-10/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase16_10
+{
+    class Persona
+    {
+        //atributos
+        //modificador de acceso / mutador /mutador de acceso
+        //los atributos por defecto son "private"
+        private string nombre;
+        private string apellido;
+        private string rut;
+        private int edad;
+
+        //ENCAPSULAR
+    }
+}
